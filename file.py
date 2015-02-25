9871def5d73caa16a3f6bd6c8693bba6a9145cb1
pipiP2P <pipiP2P@gmail.com>
2015-02-25 15:24:50 +0200

    Create file.py

---
+++ file.py
@@ -0,0 +1,14 @@
+class File:
+
+    def __init__(self, name, description, size, num_of_parts, _hash):
+        self.name = name
+        self.description = description
+        self.size = size
+        self.num_of_parts = num_of_parts
+        self._hash = _hash
+
+    def to_string(self):
+        return (self.name + ";" + self.description.encode("base64") + ";" + str(self.size) + ";" +
+                str(self.num_of_parts) + ";" + self._hash)
+
+
