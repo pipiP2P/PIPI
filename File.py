40d6a34783a192d0c0253ed73cd1af50b251968d
pipiP2P <pipiP2P@gmail.com>
2015-02-25 18:58:39 +0200

    Update and rename file.py to File.py

--- File.py
+++ File.py
@@ -1,4 +1,4 @@
-class File:
+class File_Info:
 
     def __init__(self, name, description, size, num_of_parts, _hash):
         self.name = name
@@ -8,7 +8,7 @@ class File:
         self._hash = _hash
 
     def to_string(self):
-        return (self.name + ";" + self.description.encode("base64") + ";" + str(self.size) + ";" +
+        return (self.name.encode("base64") + ";" + self.description.encode("base64") + ";" + str(self.size) + ";" +
                 str(self.num_of_parts) + ";" + self._hash)
 
 
