11ab11e5c322302bdedb8495dc7dcdf96494235d
pipiP2P <pipiP2P@gmail.com>
2015-02-26 16:16:47 +0200

    Update File.py

--- File.py
+++ File.py
@@ -11,4 +11,27 @@ class File_Info:
         return (self.name.encode("base64") + ";" + self.description.encode("base64") + ";" + str(self.size) + ";" +
                 str(self.num_of_parts) + ";" + self._hash)
 
+    
+    def get_hash(self):
+        return self._hash
 
+
+    def get_name(self):
+        return self.name
+
+
+    def get_size(self):
+        return self.size
+
+
+    def get_file_content(self, part_number):
+        # return the content of file at part number
+        file_path = "C:\\Users\\Ben\\Desktop\\PiPi\\" + self.name
+        file_object = file_path.open('rb')
+        file_content = file_object.read()
+        file_object.close()
+        return file_content[0:10]
+
+
+    def get_num_of_parts(self):
+        return self.num_of_parts
